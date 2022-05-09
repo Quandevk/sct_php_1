abf977294d0afe004fb6e40d1a73c99f7ca5ff2f
Quande <ozma.ru@mail.ru>
2022-05-09 12:59:40 +0400

    a

---
+++ lesson_2/1.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $a=5;
+    $b=2;
+    $add=$a+$b;
+    $sub=$a-$b;
+    $mult=$a*$b;
+    $div=$a/$b;
+    $exp=pow($a,$b);
+    echo 'a= '.$a,' b= '.$b, ' add= '.$add,' sub= '. $sub,' mult= '.$mult,' div= '.$div,' exp= '.$exp;
+?>
+</body>
+</html>
